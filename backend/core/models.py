af660b1a1b4baf960b2696b6af387236e63aa7d3
Francisco Jair <fjair123@ufpi.edu.br>
2022-01-18 00:46:29 -0300

    Modelagem do models de banco de dados

--- backend/core/models.py
+++ backend/core/models.py
@@ -5,5 +5,18 @@ class Cidades(models.Model):
     cidade = models.CharField("Cidade", max_length=20)
     estado = models.CharField("Estado", max_length=20)
     siglaEstado = models.CharField("Sigla do Estado", max_length=3)
+
+"""Verificar isso aqui"""
+class Tintas(models.Model):
+    nome = models.CharField("Nome", blank=False, null=False, max_length=50)
+    referencia = models.CharField("Referência", blank=False, null=False, max_length=50)
+    codigoDaCor = models.CharField("Código da cor", blank=False, null=False, max_length=50)
+    categoriaDaCor = models.CharField("Categoria da cor", blank=False, null=False, max_length=50)
+
+
+class Produtos(models.Model):
+    pass
+
+
     
     
